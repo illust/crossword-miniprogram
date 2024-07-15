a613e2f1897a1deba3edddf260680976ef788ee2
svenwong <illust0130@foxmail.com>
2024-07-15 16:57:34 +0800

    Initial Commit

---
+++ miniprogram/pages/webview/webview.ts
@@ -0,0 +1,68 @@
+// index.ts
+// 获取应用实例
+
+
+Component({
+  data: {
+    webUrl:'',
+    userId:'',
+
+    motto: 'Hello World',
+    userInfo: {
+      nickName: '',
+    },
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  },
+  methods: {
+
+onLoad: function (options: any) {
+  console.log({ 'h5传入参数': options })
+  // const newUrl = options.path +"?userId="+options.userId;
+  const newUrl = 'https://wanghao.ah.cn/home'
+  console.log('path: ',newUrl);
+  this.setData({
+    webUrl:newUrl,
+    userId:options.userId
+  })
+},
+
+    // 事件处理函数
+    bindViewTap() {
+      wx.navigateTo({
+        url: '../logs/logs',
+      })
+    },
+    onChooseAvatar(e: any) {
+      const { avatarUrl } = e.detail
+      const { nickName } = this.data.userInfo
+      this.setData({
+        "userInfo.avatarUrl": avatarUrl,
+        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+      })
+    },
+    onInputChange(e: any) {
+      const nickName = e.detail.value
+      const { avatarUrl } = this.data.userInfo
+      this.setData({
+        "userInfo.nickName": nickName,
+        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+      })
+    },
+    getUserProfile() {
+      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+      wx.getUserProfile({
+        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
+          console.log(res)
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    },
+  },
+})
+
